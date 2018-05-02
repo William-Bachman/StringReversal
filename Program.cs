64b9aa99efbc9e3338156a2166c429d228a28649
William Bachman <william_bachman@outlook.com>
2018-05-02 01:34:59 -0400

    Add files via upload
    Just a simple String reversal using linq

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Sentence");
+            String StringToReverse = Console.ReadLine();
+            if (StringToReverse != "")
+            {
+                try
+                {
+                    String ReversedString = String.Join(" ", StringToReverse.Split(' ')
+            .Select(x => new String(x.Reverse().ToArray()).ToLower()));
+                    Console.WriteLine("Reversed is:");
+                    Console.WriteLine(ReversedString);
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.ToString());
+                }
+
+            }else {
+
+                Console.WriteLine("Please Enter a sentence");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
